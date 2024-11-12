24870b67f5ef5d4a910d5076e11d3e539c4ee25d
Savita Nandan <nandan.savita@cris.org.in>
2024-11-12 15:31:30 +0530

    esrtdyui

--- lib/face_authentication/face_authentication.dart
+++ lib/face_authentication/face_authentication.dart
@@ -1,48 +1,71 @@
-import 'dart:math';
+import 'dart:io';
 import 'package:flutter/foundation.dart';
-import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class FaceAuthentication extends StatefulWidget {
   const FaceAuthentication({super.key});
 
   @override
-  State<FaceAuthentication> createState() => _FaceAuthenticationState();
+  _FaceAuthenticationState createState() => _FaceAuthenticationState();
 }
 
 class _FaceAuthenticationState extends State<FaceAuthentication> {
   late FaceDetector faceDetector;
   bool isDetecting = false;
   CameraController? cameraController;
+  CameraImage? latestImage;
 
   @override
   void initState() {
     super.initState();
-    final options = FaceDetectorOptions(enableLandmarks: true, enableClassification: true, enableTracking: true);
+    checkPermissions();
+    initializeFaceDetector();
+  }
+
+  // Check for camera permissions
+  Future<void> checkPermissions() async {
+    var status = await Permission.camera.status;
+    if (!status.isGranted) {
+      await Permission.camera.request();
+    }
+  }
+
+  // Initialize face detector
+  void initializeFaceDetector() {
+    final options = FaceDetectorOptions(
+      enableLandmarks: true,
+      enableClassification: true,
+      enableTracking: true,
+    );
     faceDetector = FaceDetector(options: options);
     initializeCamera();
   }
 
+  // Initialize camera
   Future<void> initializeCamera() async {
     final cameras = await availableCameras();
     final frontCamera = cameras.firstWhere((camera) => camera.lensDirection == CameraLensDirection.front);
-    cameraController = CameraController(frontCamera,
+    cameraController = CameraController(
+      frontCamera,
       ResolutionPreset.medium,
       enableAudio: false,
     );
 
     await cameraController!.initialize();
-
+    setState(() {});
     cameraController!.startImageStream((CameraImage image) {
       if (!isDetecting) {
         isDetecting = true;
+        latestImage = image;
         processCameraImage(image);
       }
     });
   }
 
+  // Process camera image for face detection
   Future<void> processCameraImage(CameraImage image) async {
     if (!mounted || !cameraController!.value.isStreamingImages) return;
 
@@ -50,39 +73,27 @@ class _FaceAuthenticationState extends State<FaceAuthentication> {
       final inputImage = _convertCameraImageToInputImage(image);
       final List<Face> faces = await faceDetector.processImage(inputImage);
 
-      for (Face face in faces) {
-        final Rect boundingBox = face.boundingBox;
-        final double? rotX = face.headEulerAngleX;
-        final double? rotY = face.headEulerAngleY;
-        final double? rotZ = face.headEulerAngleZ;
+      if (faces.isEmpty) {
+        print("No faces detected.");
+      } else {
+        for (Face face in faces) {
+          final boundingBox = face.boundingBox;
+          final rotX = face.headEulerAngleX;
+          final rotY = face.headEulerAngleY;
+          final rotZ = face.headEulerAngleZ;
 
-        // Example: Only print information about the first face detected
-        print("Bounding Box: $boundingBox, RotX: $rotX, RotY: $rotY, RotZ: $rotZ");
-
-        final FaceLandmark? leftEar = face.landmarks[FaceLandmarkType.leftEar];
-        if (leftEar != null) {
-          final Point<int> leftEarPos = leftEar.position;
-          print("Left ear position: $leftEarPos");
-        }
-
-        if (face.smilingProbability != null) {
-          final double smileProb = face.smilingProbability!;
-          print("Smile probability: $smileProb");
-        }
-
-        if (face.trackingId != null) {
-          final int id = face.trackingId!;
-          print("Face tracking ID: $id");
+          print("Bounding Box: $boundingBox, RotX: $rotX, RotY: $rotY, RotZ: $rotZ");
         }
       }
     } catch (e) {
       print("Error processing camera image: $e");
     } finally {
-      await Future.delayed(const Duration(milliseconds: 100)); // Adding delay to reduce frequency
+      await Future.delayed(const Duration(milliseconds: 100)); // Add delay to reduce frequency
       isDetecting = false;
     }
   }
 
+  // Convert camera image to InputImage
   InputImage _convertCameraImageToInputImage(CameraImage image) {
     final WriteBuffer allBytes = WriteBuffer();
     for (final plane in image.planes) {
@@ -93,6 +104,7 @@ class _FaceAuthenticationState extends State<FaceAuthentication> {
     const InputImageRotation rotation = InputImageRotation.rotation0deg;
     const InputImageFormat format = InputImageFormat.nv21;
     final List<int> bytesPerRow = image.planes.map((plane) => plane.bytesPerRow).toList();
+
     return InputImage.fromBytes(
       bytes: bytes,
       metadata: InputImageMetadata(
@@ -104,19 +116,28 @@ class _FaceAuthenticationState extends State<FaceAuthentication> {
     );
   }
 
+  // Capture photo
   Future<void> capturePhoto() async {
     try {
-      final image = await cameraController!.takePicture();
-      final inputImage = InputImage.fromFilePath(image.path);
+      // Capture the image
+      final XFile photo = await cameraController!.takePicture();
+      print("Captured photo path: ${photo.path}");
+
+      // Process the captured image for face detection
+      final inputImage = InputImage.fromFilePath(photo.path);
       final List<Face> faces = await faceDetector.processImage(inputImage);
-      for (Face face in faces) {
-        print("Face detected: ${face.boundingBox}");
+
+      if (faces.isEmpty) {
+        print("No faces detected.");
+      } else {
+        for (Face face in faces) {
+          print("Face detected: ${face.boundingBox}");
+        }
       }
     } catch (e) {
       print("Error capturing photo: $e");
     }
   }
-
 
   @override
   void dispose() {
@@ -132,13 +153,11 @@ class _FaceAuthenticationState extends State<FaceAuthentication> {
         centerTitle: true,
         title: const Text("Face Detector", style: TextStyle(fontWeight: FontWeight.w600)),
       ),
-      body: cameraController != null && cameraController!.value.isInitialized ?
-      CameraPreview(cameraController!) :
-      const Center(child: CircularProgressIndicator()),
+      body: cameraController != null && cameraController!.value.isInitialized
+          ? CameraPreview(cameraController!)
+          : const Center(child: CircularProgressIndicator()),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          capturePhoto();
-        },
+        onPressed: capturePhoto,
         child: const Icon(Icons.camera),
       ),
     );
